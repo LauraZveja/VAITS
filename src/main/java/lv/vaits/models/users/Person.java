4ce6130dbd96af2721f57a4ad47b95e2c47afa43
LauraZveja <lauramaisele@hotmail.lv>
2023-05-19 11:24:00 +0300

    Add: add person class

---
+++ src/main/java/lv/vaits/models/users/Person.java
@@ -0,0 +1,49 @@
+package lv.vaits.models.users;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "person_table")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Person {
+	
+	//TODO uzlikt Data JPA anotācijas
+	//TODO uzlikt atbilstošās validāciju anotācijas
+	//TODO izveidot Student, Course, Grade
+	@Setter(value = AccessLevel.NONE)
+	@Column(name = "Idp")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long idp;
+	
+	@NotNull
+	@Column(name = "Name")
+	@Size(min = 3, max =15)
+	@NotNull
+	@Pattern(regexp = "[A-ZĒŪĻĶ]{1}[a-zēūļķ]+", message = "Pirmajam burtam jābūt lielajam")
+	private String name;
+	
+	@Column(name = "Surname")
+	@Pattern(regexp = "[A-ZĒŪĻĶ]{1}[a-zēūļķ]+", message = "Pirmajam burtam jābūt lielajam")
+	@NotNull
+	private String surname;
+	
+	@Pattern(regexp = "[0-9] {6} - [0-9] {5}", message = "Neatbilstošs personas kods")
+	@NotNull
+	@Size(min = 12, max = 12)
+	//TODO apdomāt un pievienot risinājumu ārzemju studentiem un jaunajiem LV personas kodiem
+	private String personCode;
+
+}
