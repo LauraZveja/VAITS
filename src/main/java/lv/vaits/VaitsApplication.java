fb47cbcdb15a89fead38aff6790cecb4364f23cb
LauraZveja <lauramaisele@hotmail.lv>
2023-05-19 10:51:09 +0300

    first commit

---
+++ src/main/java/lv/vaits/VaitsApplication.java
@@ -0,0 +1,13 @@
+package lv.vaits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaitsApplication.class, args);
+	}
+
+}
