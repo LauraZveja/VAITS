ae2343396b0ccf28e5f6b8c4187d8d0c9c0a4d04
LauraZveja <lauramaisele@hotmail.lv>
2023-05-19 11:33:40 +0300

    Add: add inheritance annotation to person

--- src/main/java/lv/vaits/models/users/Person.java
+++ src/main/java/lv/vaits/models/users/Person.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -19,6 +21,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Person {
 	
 	//TODO uzlikt Data JPA anotācijas
