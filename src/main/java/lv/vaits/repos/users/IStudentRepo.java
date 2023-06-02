cf228cfb20a6627643719b0c906c0aebb8bd21ec
LauraZveja <lauramaisele@hotmail.lv>
2023-06-02 10:56:16 +0300

    Add: repos

---
+++ src/main/java/lv/vaits/repos/users/IStudentRepo.java
@@ -0,0 +1,9 @@
+package lv.vaits.repos.users;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.vaits.models.users.Student;
+
+public interface IStudentRepo extends CrudRepository<Student, Long>{
+
+}
