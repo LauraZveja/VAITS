b6769363343c0c7a5dc6289b4dab9033f6789c69
LauraZveja <lauramaisele@hotmail.lv>
2023-06-02 09:38:35 +0300

    Add: added Academic Staff class

--- src/main/java/lv/vaits/models/users/AcademicStaff.java
+++ src/main/java/lv/vaits/models/users/AcademicStaff.java
@@ -1,5 +1,25 @@
 package lv.vaits.models.users;
 
-public class AcademicStaff {
+import java.util.Collection;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lv.vaits.models.Course;
+
+@Table(name = "academic_table")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AttributeOverride(name = "Idp", column = @Column(name = "Ida"))
+public class AcademicStaff extends Person{
+	
+	@Column(name = "Degree")
+	private Degree degree;
 
 }
