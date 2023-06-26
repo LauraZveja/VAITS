7eeb34a82d40b6acfb264a351d3c74604d6f900b
Edgars Bērziņš <berzins.edgars.1992@gmail.com>
2023-06-26 21:22:33 +0300

    previously done in eclipse

--- src/main/java/lv/vaits/models/users/Student.java
+++ src/main/java/lv/vaits/models/users/Student.java
@@ -12,12 +12,11 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lv.vaits.models.Course;
+import lv.vaits.models.StudentStudyProgram;
 import lv.vaits.models.Thesis;
 
 @Table(name = "student_table")
@@ -26,56 +25,54 @@ import lv.vaits.models.Thesis;
 @Setter
 @NoArgsConstructor
 @AttributeOverride(name = "Idp", column = @Column(name = "Ids"))
-public class Student extends Person{
-	
-	//TODO izveidot Data JPA anotācijas
-	//TODO izveidot validāciju anotācijas
-	//TODO izveidot sasaisti ar Course klasi, ManyToMany
+public class Student extends Person {
+
+	// TODO izveidot Data JPA anotācijas
+	// TODO izveidot validāciju anotācijas
+	// TODO izveidot sasaisti ar Course klasi, ManyToMany
 	@Column(name = "MatriculaNo")
 	@NotNull
-	///@Size(min = 8, max = 20)
-	//@Pattern(regexp = "[0-9] {8,20}")
+	/// @Size(min = 8, max = 20)
+	// @Pattern(regexp = "[0-9] {8,20}")
 	private String matriculaNo;
-	
+
 	@Column(name = "FinancialDebt")
 	private boolean financialDebt;
-	
+
 	@ManyToMany
-	@JoinTable(name = "student_debt_course_table",
-	joinColumns = @JoinColumn(name = "Ids"),
-	inverseJoinColumns = @JoinColumn(name = "Idc"))
+	@JoinTable(name = "student_debt_course_table", joinColumns = @JoinColumn(name = "Ids"), inverseJoinColumns = @JoinColumn(name = "Idc"))
 	private Collection<Course> debtCourse = new ArrayList<>();
-	
+
 	@OneToMany(mappedBy = "student")
 	private Collection<Thesis> thesis;
 
-	public Student(
-			@NotNull 
-			//@Size(min = 3, max = 15) 
-			//@Pattern(regexp = "[A-ZĒŪĻĶ]{1}[a-zēūļķ]+", message = "Pirmajam burtam jābūt lielajam") 
-			String name,
-			//@Pattern(regexp = "[A-ZĒŪĻĶ]{1}[a-zēūļķ]+", message = "Pirmajam burtam jābūt lielajam") 
-			@NotNull 
-			String surname,
-			//@Pattern(regexp = "[0-9] {6} - [0-9] {5}", message = "Neatbilstošs personas kods") 
-			@NotNull 
-			//@Size(min = 12, max = 12) 
-			String personcode,
-			User user, 
-			//@NotNull @Size(min = 8, max = 20) @Pattern(regexp = "[0-9] {8,20}") 
-			String matriculaNo,
-			boolean financialDebt) {
+	@OneToMany(mappedBy = "student")
+	private Collection<StudentStudyProgram> studentStudyProgram;
+
+	public Student(@NotNull
+	// @Size(min = 3, max = 15)
+	// @Pattern(regexp = "[A-ZĒŪĻĶ]{1}[a-zēūļķ]+", message = "Pirmajam burtam jābūt
+	// lielajam")
+	String name,
+			// @Pattern(regexp = "[A-ZĒŪĻĶ]{1}[a-zēūļķ]+", message = "Pirmajam burtam jābūt
+			// lielajam")
+			@NotNull String surname,
+			// @Pattern(regexp = "[0-9] {6} - [0-9] {5}", message = "Neatbilstošs personas
+			// kods")
+			@NotNull
+			// @Size(min = 12, max = 12)
+			String personcode, User user,
+			// @NotNull @Size(min = 8, max = 20) @Pattern(regexp = "[0-9] {8,20}")
+			String matriculaNo, boolean financialDebt) {
 		super(name, surname, personcode, user);
 		this.matriculaNo = matriculaNo;
 		this.financialDebt = financialDebt;
 	}
-	
+
 	public void addDebtCourse(Course course) {
-		if(!debtCourse.contains(course)) {
+		if (!debtCourse.contains(course)) {
 			debtCourse.add(course);
 		}
 	}
-	
-	
 
 }
