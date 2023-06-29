a0d6bbe18c3dc960803c5b57173358609cfa31da
Laura <lauramaisele@hotmail.lv>
2023-06-29 23:33:13 +0300

    New ICommentsServices

--- src/main/java/lv/vaits/services/ICommentsServices.java
+++ src/main/java/lv/vaits/services/ICommentsServices.java
@@ -1,5 +1,19 @@
 package lv.vaits.services;
 
+import java.util.ArrayList;
+
+import lv.vaits.models.Comment;
+import lv.vaits.models.Thesis;
+import lv.vaits.models.users.AcademicStaff;
+
 public interface ICommentsServices {
+
+	Comment createNewComment(String description, AcademicStaff staff, Thesis thesis);
+
+	Comment updateCommentById(Long id, String description, AcademicStaff staff, Thesis thesis) throws Exception;
+
+	void deleteCommentById(Long id) throws Exception;
+
+	ArrayList<Comment> selectAllByThesisId(Long id);
 
 }
