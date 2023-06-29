2c1b48cc89adbd414643c7925dd47bde5c6894cf
Laura <lauramaisele@hotmail.lv>
2023-06-29 20:42:50 +0300

    Added theses update status page and function

--- src/main/java/lv/vaits/controllers/ThesisController.java
+++ src/main/java/lv/vaits/controllers/ThesisController.java
@@ -5,10 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.validation.Valid;
+import lv.vaits.models.AcceptanceStatus;
 import lv.vaits.models.Thesis;
 import lv.vaits.services.ICommentsServices;
 import lv.vaits.services.IThesisServices;
@@ -108,22 +111,45 @@ public class ThesisController {
 	public String changeSupervisorByThesisAndSupervisorIdGetFunc(@PathVariable("idThesis") Long id,
 			@PathVariable("idSupervisor") Long idSupervisor, Model model) {
 		try {
-			model.addAttribute("allThesis",
-					thesisServices.changeSupervisorByThesisAndSupervisorId(id, idSupervisor));
+			model.addAttribute("allThesis", thesisServices.changeSupervisorByThesisAndSupervisorId(id, idSupervisor));
 			return "thesis-all-page";
 		} catch (Exception e) {
 			return "error-page";
 		}
 	}
-	
+
 	@GetMapping("/thesis/addReviewerByThesisId/{idThesis}/{idReviewer}")
-	public String addReviewerByThesisId(@PathVariable("idThesis") Long id,
-			@PathVariable("idReviewer") Long idReviewer, Model model) {
+	public String addReviewerByThesisId(@PathVariable("idThesis") Long id, @PathVariable("idReviewer") Long idReviewer,
+			Model model) {
 		try {
-			model.addAttribute("allThesis",
-					thesisServices.addReviewerByThesisId(id, idReviewer));
+			model.addAttribute("allThesis", thesisServices.addReviewerByThesisId(id, idReviewer));
 			return "thesis-all-page";
 		} catch (Exception e) {
+			return "error-page";
+		}
+	}
+
+	@GetMapping("/thesis/updateStatus/{id}")
+	public String updateThesisStatusGetFunc(@PathVariable("id") Long id, Model model) {
+		try {
+			model.addAttribute("thesis", thesisServices.retrieveThesisById(id));
+			return "thesis-update-status-page";
+		} catch (Exception e) {
+			return "error-page";
+		}
+	}
+
+	@PostMapping("/thesis/updateStatus/{id}")
+	public String updateThesisStatusPostFunc(@PathVariable("id") Long id, @Valid Thesis thesis,
+			BindingResult result) {
+		if (!result.hasErrors()) {
+			try {
+				Thesis updatedThesis = thesisServices.updateThesisStatus(id, thesis.getAccStatus());
+				return "redirect:/thesis/showAll/" + updatedThesis.getIdt();
+			} catch (Exception e) {
+				return "redirect:/thesis/error";
+			}
+		} else {
 			return "error-page";
 		}
 	}
