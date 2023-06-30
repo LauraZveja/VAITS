b24ba42dbdb16fb1f3993f7ddd9960965e5478f4
Laura <lauramaisele@hotmail.lv>
2023-06-30 10:48:04 +0300

    Added export thesis to excel functionality

--- src/main/java/lv/vaits/controllers/ThesisController.java
+++ src/main/java/lv/vaits/controllers/ThesisController.java
@@ -8,12 +8,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lv.vaits.models.Thesis;
 import lv.vaits.repos.IThesisRepo;
 import lv.vaits.services.IThesisServices;
 import lv.vaits.services.users.IAcademicStaffServices;
 import lv.vaits.services.users.IStudentServices;
+
+import java.io.IOException;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 @Controller
 public class ThesisController {
@@ -150,5 +158,54 @@ public class ThesisController {
 			return "error-page";
 		}
 	}
+	
+	 @GetMapping("/thesis/export")
+	    public void exportThesesToExcel(HttpServletResponse response) throws IOException {
+	        List<Thesis> theses = thesisServices.retrieveActiveTheses();
+
+	        // Create a new workbook
+	        Workbook workbook = new XSSFWorkbook();
+
+	        // Create a sheet
+	        Sheet sheet = workbook.createSheet("Theses");
+
+	        // Create a header row
+	        Row headerRow = sheet.createRow(0);
+	        headerRow.createCell(0).setCellValue("Title (LV)");
+	        headerRow.createCell(1).setCellValue("Title (EN)");
+	        headerRow.createCell(2).setCellValue("Aim");
+	        headerRow.createCell(3).setCellValue("Tasks");
+	        headerRow.createCell(4).setCellValue("Student");
+	        headerRow.createCell(5).setCellValue("Supervisor");
+
+	        // Create data rows
+	        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+	        int rowNum = 1;
+	        for (Thesis thesis : theses) {
+	            Row dataRow = sheet.createRow(rowNum++);
+	            dataRow.createCell(0).setCellValue(thesis.getTitleLv());
+	            dataRow.createCell(1).setCellValue(thesis.getTitleEn());
+	            dataRow.createCell(2).setCellValue(thesis.getAim());
+	            dataRow.createCell(3).setCellValue(thesis.getTasks());
+	            dataRow.createCell(4).setCellValue(thesis.getStudent().getName() + " " + thesis.getStudent().getSurname());
+	            dataRow.createCell(5).setCellValue(thesis.getSupervisor().getName() + " " + thesis.getSupervisor().getSurname());
+	        }
+
+	        // Set column widths (optional)
+	        sheet.setColumnWidth(0, 8000);
+	        sheet.setColumnWidth(1, 8000);
+	        sheet.setColumnWidth(2, 8000);
+	        sheet.setColumnWidth(3, 8000);
+	        sheet.setColumnWidth(4, 8000);
+	        sheet.setColumnWidth(5, 8000);
+
+	        // Set the response headers
+	        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+	        response.setHeader("Content-Disposition", "attachment; filename=theses.xlsx");
+
+	        // Write the workbook to the response output stream
+	        workbook.write(response.getOutputStream());
+	        workbook.close();
+	    }
 
 }
