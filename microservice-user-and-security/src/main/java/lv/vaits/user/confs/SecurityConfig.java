8c74fc5dc48da28c80a7a620b5d95a4b1a03eb1a
Katrina <katrina.sleze@gmail.com>
2023-10-20 14:06:20 +0300

    Added i18n

--- microservice-user-and-security/src/main/java/lv/vaits/user/confs/SecurityConfig.java
+++ microservice-user-and-security/src/main/java/lv/vaits/user/confs/SecurityConfig.java
@@ -6,12 +6,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
 import jakarta.servlet.DispatcherType;
 import lv.vaits.user.services.users.impl.MyUserDetailsManagerImpl;
 
