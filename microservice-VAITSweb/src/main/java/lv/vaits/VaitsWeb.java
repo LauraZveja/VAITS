62c40702fc6476337040c09e5d479da52805b42c
LauraZveja <lauramaisele@hotmail.lv>
2023-10-20 13:20:57 +0300

    Update: change permissions for remove reviewer id

--- microservice-VAITSweb/src/main/java/lv/vaits/VaitsWeb.java
+++ microservice-VAITSweb/src/main/java/lv/vaits/VaitsWeb.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 import lv.vaits.models.Comment;
