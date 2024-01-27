e8f041d8854da84f0d8d5352c36118dc5b01db7f
Laura <lauramaisele@hotmail.lv>
2024-01-27 16:58:52 +0200

    Add: Comment model unit tests for getters and setters

--- microservice-VAITSweb/src/test/java/lv/vaits/models/CommentTest.java
+++ microservice-VAITSweb/src/test/java/lv/vaits/models/CommentTest.java
@@ -26,4 +26,55 @@ class CommentTest {
     }
 
 
+    @Test
+    void getIdco() {
+        assertEquals(0, goodComment.getIdco());
+    }
+
+    @Test
+    void getDescription() {
+        assertEquals("Good comment", goodComment.getDescription());
+    }
+
+    @Test
+    void getCommentDate() {
+        assertEquals(commentDate.truncatedTo(ChronoUnit.SECONDS), goodComment.getCommentDate().truncatedTo(ChronoUnit.SECONDS));
+    }
+
+    @Test
+    void getStaff() {
+        assertEquals(staff, goodComment.getStaff());
+    }
+
+    @Test
+    void getThesis() {
+        assertEquals(thesis, goodComment.getThesis());
+    }
+
+    @Test
+    void setDescription() {
+        goodComment.setDescription("Test description");
+        assertEquals("Test description", goodComment.getDescription());
+    }
+
+    @Test
+    void setCommentDate() {
+        LocalDateTime newDate = LocalDateTime.now().plusDays(1);
+        goodComment.setCommentDate(newDate);
+        assertEquals(newDate.truncatedTo(ChronoUnit.SECONDS), goodComment.getCommentDate().truncatedTo(ChronoUnit.SECONDS));
+    }
+
+    @Test
+    void setStaff() {
+        AcademicStaff newStaff = new AcademicStaff();
+        goodComment.setStaff(newStaff);
+        assertEquals(newStaff, goodComment.getStaff());
+    }
+
+    @Test
+    void setThesis() {
+        Thesis newThesis = new Thesis();
+        goodComment.setThesis(newThesis);
+        assertEquals(newThesis, goodComment.getThesis());
+    }
 }
