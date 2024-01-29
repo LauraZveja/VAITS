15c80b68e079a119d980b33edbbd168a8eb427e9
Laura <lauramaisele@hotmail.lv>
2024-01-29 23:17:01 +0200

    Edit: fixed CommentTest unit tests

--- microservice-VAITSweb/src/test/java/lv/vaits/models/CommentTest.java
+++ microservice-VAITSweb/src/test/java/lv/vaits/models/CommentTest.java
@@ -1,20 +1,15 @@
 package lv.vaits.models;
 
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Validation;
-import jakarta.validation.Validator;
-import jakarta.validation.ValidatorFactory;
 import lv.vaits.models.users.AcademicStaff;
-import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import lv.vaits.models.users.Student;
+import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CommentTest {
+class CommentTest {
     AcademicStaff staff = new AcademicStaff();
     Thesis thesis = new Thesis();
     LocalDateTime commentDate = LocalDateTime.now();
@@ -22,7 +17,7 @@ public class CommentTest {
     Comment goodComment = new Comment("Good comment", staff, thesis);
 
     @Test
-    public void testConstructor() {
+    void testConstructor() {
         assertEquals("Good comment", goodComment.getDescription());
         assertEquals(staff, goodComment.getStaff());
         assertEquals(thesis, goodComment.getThesis());
@@ -32,52 +27,52 @@ public class CommentTest {
 
 
     @Test
-    public void getIdco() {
+    void getIdco() {
         assertEquals(0, goodComment.getIdco());
     }
 
     @Test
-    public void getDescription() {
+    void getDescription() {
         assertEquals("Good comment", goodComment.getDescription());
     }
 
     @Test
-    public void getCommentDate() {
+    void getCommentDate() {
         assertEquals(commentDate.truncatedTo(ChronoUnit.SECONDS), goodComment.getCommentDate().truncatedTo(ChronoUnit.SECONDS));
     }
 
     @Test
-    public void getStaff() {
+    void getStaff() {
         assertEquals(staff, goodComment.getStaff());
     }
 
     @Test
-    public void getThesis() {
+    void getThesis() {
         assertEquals(thesis, goodComment.getThesis());
     }
 
     @Test
-    public void setDescription() {
+    void setDescription() {
         goodComment.setDescription("Test description");
         assertEquals("Test description", goodComment.getDescription());
     }
 
     @Test
-    public void setCommentDate() {
+    void setCommentDate() {
         LocalDateTime newDate = LocalDateTime.now().plusDays(1);
         goodComment.setCommentDate(newDate);
         assertEquals(newDate.truncatedTo(ChronoUnit.SECONDS), goodComment.getCommentDate().truncatedTo(ChronoUnit.SECONDS));
     }
 
     @Test
-    public void setStaff() {
+    void setStaff() {
         AcademicStaff newStaff = new AcademicStaff();
         goodComment.setStaff(newStaff);
         assertEquals(newStaff, goodComment.getStaff());
     }
 
     @Test
-    public void setThesis() {
+    void setThesis() {
         Thesis newThesis = new Thesis();
         goodComment.setThesis(newThesis);
         assertEquals(newThesis, goodComment.getThesis());
