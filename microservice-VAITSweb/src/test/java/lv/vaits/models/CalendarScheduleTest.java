dd359c09a0cd12cf9aabe132fc2e472f519f3daa
Laura <lauramaisele@hotmail.lv>
2024-01-30 12:16:36 +0200

    ADD: unit tests for CalendarScheduleTest and OtherApplicationTest

---
+++ microservice-VAITSweb/src/test/java/lv/vaits/models/CalendarScheduleTest.java
@@ -0,0 +1,79 @@
+package lv.vaits.models;
+
+import lv.vaits.models.users.AcademicStaff;
+import lv.vaits.models.users.Student;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalendarScheduleTest {
+
+    StudyProgram goodStudyProgram = new StudyProgram("Study program", Faculty.ITF, LevelOfStudy.FIRST_LEVEL);
+
+    CalendarSchedule goodCalendarSchedule = new CalendarSchedule(2024, "Defense", LocalDate.now(), goodStudyProgram);
+
+    StudyProgram studyProgram = new StudyProgram();
+
+    CalendarSchedule calendarSchedule = new CalendarSchedule();
+
+    @Test
+    void getIdcs() {
+        assertEquals(0, goodCalendarSchedule.getIdcs());
+    }
+
+    @Test
+    void getStudyYear() {
+        assertEquals(2024, goodCalendarSchedule.getStudyYear());
+
+    }
+
+    @Test
+    void setStudyYear() {
+        calendarSchedule.setStudyYear(2025);
+        assertEquals(2025, calendarSchedule.getStudyYear());
+    }
+
+    @Test
+    void getActivity() {
+        assertEquals("Defense", goodCalendarSchedule.getActivity());
+    }
+
+    @Test
+    void setActivity() {
+        calendarSchedule.setActivity("Graduation");
+        assertEquals("Graduation", calendarSchedule.getActivity());
+    }
+
+    @Test
+    void getDeadline() {
+        assertEquals(LocalDate.now(), goodCalendarSchedule.getDeadline());
+
+    }
+
+    @Test
+    void setDeadline() {
+        calendarSchedule.setDeadline(LocalDate.now().plusDays(1));
+        assertEquals(LocalDate.now().plusDays(1), calendarSchedule.getDeadline());
+
+    }
+
+    @Test
+    void getStudyProgram() {
+        assertEquals(goodStudyProgram, goodCalendarSchedule.getStudyProgram());
+
+    }
+
+    @Test
+    void setStudyProgram() {
+        calendarSchedule.setStudyProgram(studyProgram);
+        assertEquals(studyProgram, calendarSchedule.getStudyProgram());
+
+    }
+
+}
---
+++ microservice-VAITSweb/src/test/java/lv/vaits/models/OtherApplicationsTest.java
@@ -0,0 +1,65 @@
+package lv.vaits.models;
+
+import lv.vaits.models.users.AcademicStaff;
+import lv.vaits.models.users.Student;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OtherApplicationsTest {
+
+    AcademicStaff supervisor = new AcademicStaff();
+    Student student = new Student();
+    Thesis testThesis = new Thesis("Title LV", "Title EN", "Aim", "Tasks", student, supervisor);
+
+    OtherApplications otherApplications = new OtherApplications();
+
+    OtherApplications goodApplication = new OtherApplications(ApplicationType.CHANGE_THESIS_TOPIC, "Activity", testThesis);
+
+    Thesis thesis = new Thesis();
+
+    @Test
+    void getIdoa() {
+        assertEquals(0, otherApplications.getIdoa());
+    }
+
+    @Test
+    void getApplicationType() {
+        assertEquals(ApplicationType.CHANGE_THESIS_TOPIC, goodApplication.getApplicationType());
+    }
+
+    @Test
+    void getActivity() {
+        assertEquals("Activity", goodApplication.getActivity());
+    }
+
+    @Test
+    void getThesis() {
+        assertEquals(testThesis, goodApplication.getThesis());
+    }
+
+    @Test
+    void setApplicationType() {
+        otherApplications.setApplicationType(ApplicationType.OTHER);
+        assertEquals(ApplicationType.OTHER, otherApplications.getApplicationType());
+    }
+
+    @Test
+    void setActivity() {
+        otherApplications.setActivity("Activity");
+        assertEquals("Activity", otherApplications.getActivity());
+    }
+
+    @Test
+    void setThesis() {
+        otherApplications.setThesis(thesis);
+        assertEquals(thesis, otherApplications.getThesis());
+    }
+
+
+}
